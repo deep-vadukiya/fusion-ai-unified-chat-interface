a57a7a9afb6b10bdda26f990f7e61bd85b8114d2
deep <dipvadukiya06@gmail.com>
2026-07-23 16:01:03 +0200

    added console logs on user requests and added health check api to check if the server is responsding correctly or not

--- fusionai-backend/src/app.js
+++ fusionai-backend/src/app.js
@@ -17,4 +17,18 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  // log user requests to the console ...
+  console.log("➡️ ➡️", req.method, " :", req.url);
+  next();
+});
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "FusionAI Backend is running",
+    timestamp: new Date().toISOString(),
+  });
+});
+
 export default app;
--- fusionai-backend/src/server.js
+++ fusionai-backend/src/server.js
@@ -1,7 +1,9 @@
 //
 
 import dotenv from "dotenv";
+
 dotenv.config();
+
 import app from "./app.js";
 
 // ----------------------------------------------
