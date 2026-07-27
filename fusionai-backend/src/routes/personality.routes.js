a445e63338607079e2a82e11f70cab5cebe5718f
deep <dipvadukiya06@gmail.com>
2026-07-27 18:36:11 +0200

    added personality crud routes and matched with respective controller

--- fusionai-backend/src/routes/personality.routes.js
+++ fusionai-backend/src/routes/personality.routes.js
@@ -1,12 +1,17 @@
 //
 
 import { Router } from "express";
-import * as PersonalityController from "../controllers/PersonalityController.js";
+import * as personalityController from "../controllers/PersonalityController.js";
 
 // ----------------------------------------------
 
 const router = Router();
 
-router.post("/", PersonalityController.createPersonality);
+router.post("/", personalityController.createPersonality);
+router.put("/:id", personalityController.updatePersonality);
+router
+  .get("/", personalityController.getAllPersonalities)
+  .get("/:id", personalityController.getPersonalityById);
+router.delete("/:id", personalityController.deletePersonality);
 
 export default router;
