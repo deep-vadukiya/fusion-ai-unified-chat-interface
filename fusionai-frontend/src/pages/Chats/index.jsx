ab441fd6831619a518069a5b098dc51b8d87bfd5
deep <dipvadukiya06@gmail.com>
2026-08-06 14:22:18 +0200

    added MUI style library to the app with basic ui configuration

--- fusionai-frontend/src/pages/Chats/index.jsx
+++ fusionai-frontend/src/pages/Chats/index.jsx
@@ -1,13 +1,52 @@
 //
 
 import { Outlet } from "react-router-dom";
+import { Container, Grid } from "@mui/material";
 
 // ------------------------------------------------
 
 export default function index() {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Container
+      disableGutters={true}
+      maxWidth={false}
+    >
+      <Grid
+        container
+        spacing={2}
+      >
+        <Grid
+          size={2}
+          sx={{
+            borderRight: "1px solid #ccc",
+            maxHeight: "100vh",
+            overflowY: "auto",
+            padding: 1,
+          }}
+        >
+          <ChatsList />
+        </Grid>
+
+        <Grid
+          size={10}
+          sx={{
+            borderRight: "1px solid #ccc",
+            maxHeight: "100vh",
+            overflowY: "auto",
+            padding: 1,
+            backgroundColor: "#f5f5f5",
+          }}
+        >
+          <Outlet />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
+
+//
+const ChatsList = () => {
+  //
+
+  return <p>Chats section ....</p>;
+};
